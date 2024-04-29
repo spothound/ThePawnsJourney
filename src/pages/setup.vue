04d707ac82b190241e5c9c4b87559cf072240b66
Francisco Navarro Morales <francisco.morales@preply.com>
2024-04-29 19:41:07 +0200

    Improve setup page

--- src/pages/setup.vue
+++ src/pages/setup.vue
@@ -11,7 +11,7 @@ definePage({
   },
 })
 const isRangeInvalid = ref(false) // Add this line
-const numberOfPuzzles = ref(15)
+const numberOfPuzzles = ref(100)
 const eloRange = ref([0, 400])
 const submitForm = async () => {
   const getPuzzles = async (eloMin: number, eloMax: number, numberOfPuzzles: number) => {
@@ -29,10 +29,16 @@ const submitForm = async () => {
   }
 }
 
-const predefinedRanges = [[0, 500], [500, 1000], [1000, 1500], [1500, 2000], [2000, 2500], [2500, 3000], [3000, 3500]]
+const predefinedRangesElo = [[0, 500], [500, 1000], [1000, 1500], [1500, 2000], [2000, 2500], [2500, 3000], [3000, 3500]]
+const predefinedRangesN = [1, 10, 20, 50, 100, 200, 300]
 
-const selectRange = (range: number[]) => {
+
+const selectELORange = (range: number[]) => {
   eloRange.value = range
+}
+
+const selectNRange = (npuzzles: number) => {
+  numberOfPuzzles.value = npuzzles
 }
 
 </script>
@@ -41,11 +47,14 @@ const selectRange = (range: number[]) => {
   <v-sheet width="80%" class="mx-auto">
     <v-form ref="form">
       <label class="form-label">Number of puzzles:</label>
-      <v-slider v-model="numberOfPuzzles" min="10" max="300" step="10" thumb-label="always"></v-slider>
+      <v-slider v-model="numberOfPuzzles" min="1" max="300" step="1" thumb-label="always"></v-slider>
+      <v-btn v-for="(npuzzles, index) in predefinedRangesN" :key="index" class="range-button" @click="selectNRange(npuzzles)">
+          {{ npuzzles }}
+        </v-btn>
       <label class="form-label">ELO range:</label>
       <v-range-slider v-model="eloRange" min="0" max="3500" step="1" thumb-label="always" />
       <div class="predefined-ranges">
-        <v-btn v-for="(range, index) in predefinedRanges" :key="index" class="range-button" @click="selectRange(range)">
+        <v-btn v-for="(range, index) in predefinedRangesElo" :key="index" class="range-button" @click="selectELORange(range)">
           {{ range[0] }} - {{ range[1] }}
         </v-btn>
       </div>
