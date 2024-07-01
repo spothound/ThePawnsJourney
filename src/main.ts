fe3900aec0f1de8ec3ae6c01492b3f01b6f06794
Francisco Navarro Morales <francisco.morales@preply.com>
2024-07-01 23:06:14 +0200

    Add dummy request to API on startup page to bring it up if needed

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { createApp, type Plugin } from 'vue'
 import App from './App.vue'
 import '@/assets/styles/index.css'
 import vue3GoogleLogin from 'vue3-google-login'
+const apiUrl = import.meta.env.VUE_APP_API_URL || 'https://api.thepawnsjourney.live';
+
 
 const app = createApp(App)
 
@@ -15,5 +17,8 @@ Object.values(
 app.use(vue3GoogleLogin, {
   clientId: '368514305696-6nuvllac0utemf8ad4ch38j6h002ih3g.apps.googleusercontent.com'
 })
+
+// make a dummy request to the API to bring it up if needed
+fetch(`${apiUrl}/`)
 
 app.mount('#app')
